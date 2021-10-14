f0260273f0e6e5cab83584358076e3606c7a5186
wwfirsov <wwfirsov@mail.ru>
2021-10-14 16:33:03 +0300

    1

--- django/geekshop/geekshop/settings.py
+++ django/geekshop/geekshop/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['geekshop/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
